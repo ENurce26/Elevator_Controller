c5246f0b86cb417973021446707b96298bee0264
ENurce26 <47866514+ENurce26@users.noreply.github.com>
2023-04-18 16:07:25 -0400

    Update simulator.vhd

--- simulator.vhd
+++ simulator.vhd
@@ -20,14 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 entity simulator is
 	Port ( POC : in STD_LOGIC;
 	SYSCLK : in STD_LOGIC;
@@ -39,22 +32,6 @@ entity simulator is
 	EF : out STD_LOGIC_VECTOR (3 downto 0));
 end simulator;
 
-entity elevator_simulator is
-    Port ( POC : in  STD_LOGIC;
-           SYSCLK : in  STD_LOGIC;
-           EMVUP : in  STD_LOGIC;
-           EMVDN : in  STD_LOGIC;
-           EOPEN : in  STD_LOGIC;
-           ECLOSE : in  STD_LOGIC;
-           ECOMP : buffer  STD_LOGIC;
-           EF : out  STD_LOGIC_VECTOR (3 downto 0));
-end elevator_simulator;
-
-architecture Behavioral of elevator_simulator is
-	 signal ELEVATOR_DOOR : STD_LOGIC;
-    signal ELEVATOR_INPUT : STD_LOGIC;
-    signal FLOOR_NUM : UNSIGNED(3 downto 0);
-    signal OPERATION_COUNTER : UNSIGNED(2 downto 0);
 architecture Behavioral of simulator is
 	signal EDOOR, EINPUT : STD_LOGIC;
 	signal EFLOOR : UNSIGNED(3 downto 0);
